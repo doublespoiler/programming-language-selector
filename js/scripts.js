e0d19b1ecceb184c869405506da2f5aa07a111cf
Skylan Lew <skylanslew@gmail.com>
2022-08-12 10:52:39 -0700

    WIP begin writing UI logic, add more buttons to divs (may remove later)

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,49 @@
 // Business logic
 
+function getUserInput(event)
+
   //branching for suggestion goes here
 
 // UI logic
 
-function handleSubmission(event){
+function handleSubmission(event, ){
   event.preventDefault();
   //do the thing
 }
 
+// function handleReset(event){
+//   event.preventDefault();
+
+// }
+
+// function handleNext(event){
+//   event.preventDefault();
+//   if (document.)
+
+// }
+
+// function handleBack(event, currentActive){
+//   event.preventDefault();
+//   currentActive -= 1;
+//   return currentActive;
+// }
+
+// function setCurrent(currentActive){
+//   document.getElementById("question").classList = "currentActive";
+//   currentActive = 
+// }
+
+
+function suggestLanguage(question1, question2, question3, question4, question5) {
+  let difficultyMod = 0 + question1 + question2 + question3 + question4 + question5;
+
+}
+
 window.addEventListener("load", function() {
-  const submit = document.getElementById("selectorForm");
+  let currentActive = "question0"; 
+
+  const currentForm = document.querySelector("div.currentActive");
+
+  currentActive = submit.addEventListener("click", handleBack);
   submit.addEventListener("submit", handleSubmission);
 });
