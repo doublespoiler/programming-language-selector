e348294cf538a68dd5732fbedb548fa28b9736c3
Skylan Lew <skylanslew@gmail.com>
2022-08-12 15:17:21 -0700

    WIP got forward working, added error text and back button

--- js/scripts.js
+++ js/scripts.js
@@ -27,16 +27,18 @@ function handleSubmission(event){
 
 
 
-window.addEventListener("load", function(event) {
+window.addEventListener("DOMContentLoaded", function(event) {
   event.preventDefault();
   let currentShow = 0;
   let currentQuestion = "question0"; 
 
   const submit = document.getElementById("suggest-form");
   const reset = document.getElementById("reset-button");
-  const nextButton = document.getElementById ("next-button");
+  const nextButton = document.getElementById("next-button");
+  const backButton = document.getElementById("back-button");
   submit.addEventListener("submit", suggestLanguage);
-  nextButton.addEventListener("click", showNext);
+  nextButton.addEventListener("click", goNext);
+  backButton.addEventListener("click", goBack);
   reset.addEventListener("reset", resetForm);
 
   // back.addEventListener("click", goBack());
@@ -44,15 +46,45 @@ window.addEventListener("load", function(event) {
   // reset.addEventListener("reset", handleSubmission);
 });
 
-function showNext() {
-  let currentDiv = document.querySelector(".select"); 
-  let selectedDiv = currentDiv.getAttribute("id");
-  document.getElementById("question").innerText = currentDiv.innerText();
-  currentDiv.classList.remove("selected");
-  selectedDiv +=  1;
-  document.getElementById("question" + selectedDiv).classList.add("selected");
-
+function goNext() {
+  let questionDiv = document.getElementById("activeQuestion");
+  let selectedDiv = document.querySelector("div.current-active"); 
+  let currentDiv = parseInt(selectedDiv.getAttribute("id").replace("question", ""));
+  let errorText = document.querySelector("span.error-text");
+  if (currentDiv >= 5) {
+    errorText.className = "";
+    errorText.innerText = "This is the last page, please go back, reset, or submit!";
+    return;
+  } else {
+  let replaceHTML = selectedDiv.innerHTML;
+  questionDiv.innerHTML = replaceHTML;
+  selectedDiv.className = "hidden"
+  currentDiv +=  1;
+  document.getElementById("question" + currentDiv).className = "current-active";
+  errorText.className = "hidden";
+  }
 }
+
+function goBack() {
+  let questionDiv = document.getElementById("activeQuestion");
+  let selectedDiv = document.querySelector("div.current-active"); 
+  let currentDiv = parseInt(selectedDiv.getAttribute("id").replace("question", ""));
+  let errorText = document.getElementById("error-text");
+  if (currentDiv <= 0) {
+    errorText.className = "";
+    errorText.innerText = "This is the first page, you cannot go back."
+    return;
+  } else {
+  let replaceHTML = selectedDiv.innerHTML;
+  questionDiv.innerHTML = replaceHTML;
+  selectedDiv.className = "hidden"
+  currentDiv -=  1;
+  document.getElementById("question" + currentDiv).className = "current-active";
+  errorText.className = "hidden";
+  }
+}
+
+
 
 function suggestLanguage() {
   let difficultyMod = 0 + question1 + question2 + question3 + question4 + question5;
