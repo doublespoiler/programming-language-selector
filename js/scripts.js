d840f786e17381334bef71f5fa2e592bfc41fadd
Skylan Lew <skylanslew@gmail.com>
2022-08-13 15:12:31 -0700

    switched from using .innerHTML to just floating all the divs and hiding/showing them, which fixed my form input values grabbing correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,6 @@ function handleSubmission(event){
   let errorText = document.getElementById("error-text");
 
   const q1Value = document.getElementById("question1-input").value;
-  const q1 = document.getElementById("question1-input");
   const q2Value = document.getElementById("question2-input").value;
   const q3Value = document.getElementById("question3-input").value;
   const q4Value = document.getElementById("question4-input").value;
@@ -23,6 +22,9 @@ function handleSubmission(event){
   const q4Number = parseInt(q4Value);
   const q5Number = parseInt(q5Value);
 
+  const question5Div = document.getElementById("question5");
+  const resultDiv = document.getElementById("question6");
+
   const nextButton = document.getElementById("next-button");
   const submitButton = document.getElementById("submit-button");
 
@@ -30,6 +32,8 @@ function handleSubmission(event){
   nextButton.className = "hidden";
   submitButton.className = "hidden";
   errorText.className = "hidden";
+  question5Div.className = "hidden";
+  resultDiv.className = "current-active"
   suggestLanguage(q1Number, q3Number, q5Number);
 }
 // function handleReset(event){
@@ -55,11 +59,7 @@ window.addEventListener("DOMContentLoaded", function(event) {
   event.preventDefault();
   
   const rootDiv = document.getElementById("question0");
-  const questionDiv = document.getElementById("activeQuestion");
-  const replaceHTML = rootDiv.innerHTML;
   rootDiv.className = "current-active"
-  questionDiv.innerHTML = replaceHTML;
-
 
   const submit = document.getElementById("suggest-form");
   const reset = document.getElementById("reset-button");
@@ -71,7 +71,6 @@ window.addEventListener("DOMContentLoaded", function(event) {
 
 function goNext() {
   const errorText = document.getElementById("error-text");
-  const questionDiv = document.getElementById("activeQuestion");
   const nextButton = document.getElementById("next-button");
   const resetButton = document.getElementById("reset-button");
   let selectedDiv = document.querySelector("div.current-active"); 
@@ -81,46 +80,70 @@ function goNext() {
   nextButton.innerText = "Next";
   resetButton.className = "";
   if (selectedDiv.id === "question0"){
-    nextDiv = document.getElementById("question1");
-    const replaceHTML = nextDiv.innerHTML;
-    questionDiv.innerHTML = replaceHTML;
     selectedDiv.className = "hidden";
-    document.getElementById("question2").className = "current-active";
-  } else if (expectedNextDiv < 6){
-    if (selectedDiv.id === "question5") {
-      const submitButton = document.getElementById("submit-button");
-      submitButton.className = "";
-      nextButton.className = "hidden";
-    }
+    document.getElementById("question1").className = "current-active";
+  // } else if (expectedNextDiv === 5){
+  //   if (selectedDiv.id === "question5") {
+  //     const submitButton = document.getElementById("submit-button");
+  //     document.getElementById("question5").className = "current-active floating";
+  //     submitButton.className = "";
+  //     nextButton.className = "hidden";
+  //   }
+  } else if (selectedDiv.id === "question4") {
+    selectedDiv.className = "hidden"
+    const submitButton = document.getElementById("submit-button");
+    document.getElementById("question5").className = "current-active";
+    submitButton.className = "";
+    nextButton.className = "hidden";
+  } else {
     nextDiv = parseInt(selectedDiv.getAttribute("id").replace("question", ""));
-    let replaceHTML = selectedDiv.innerHTML;
-    questionDiv.innerHTML = replaceHTML;
     selectedDiv.className = "hidden"
     nextDiv += 1;
     document.getElementById("question" + nextDiv).className = "current-active";
-  } else {
-    errorText.innerText = "This is the last page! Please submit, reset, or go back!"
-    errorText.className = "";
-  }
+    }
+  // } else {
+  //   errorText.innerText = "This is the last page! Please submit, reset, or go back!"
+  //   errorText.className = "";
+  // }
 }
 
 function suggestLanguage(q1Number, q3Number, q5Number) {
-  const questionDiv = document.getElementById("activeQuestion");
+  const resultDiv = document.getElementById("question6");
+  const resultImage = document.getElementById("result-image");
+  const resultName = document.getElementById("result-name");
+  const resultText = document.getElementById("result-text");
+
 
   if (q1Number === 0){ //no experience
     if(q3Number === 1){ // no system any
       console.log("py");
+      resultImage.src = "python.png";
+      resultName.innerText = "PYTHON";
+      resultText.innerText = "asdfasdf";
+      
     } else { //no NOTsystem any
       console.log("htmlcssjs");
+      resultImage.src = "html.png";
+      resultName.innerText = "HTML / CSS / Javascript";
+      resultText.innerText = "asdfasdf";
     }
   } else if (q1Number === 1) { //little experience
     if (q3Number === 0) { //little user any
       console.log("ruby");
+      resultImage.src = "ruby.png";
+      resultName.innerText = "Ruby";
+      resultText.innerText = "asdfasdf";
     } else if (q3Number === 2) { //little games any
       console.log("c#");
+      resultImage.src = "csharp.png";
+      resultName.innerText = "C#";
+      resultText.innerText = "asdfasdf";
     } else { 
       if (q5Number = 3)  { //little system games
         console.log("c#");
+        resultImage.src = "csharp.png";
+        resultName.innerText = "C#";
+        resultText.innerText = "asdfasdf";
       } else { //little system social/tinker
         console.log("rust");
       }
@@ -128,18 +151,33 @@ function suggestLanguage(q1Number, q3Number, q5Number) {
   } else { //pro
     if (q3Number === 0) { 
       if (q5Number === 2){ //pro user tinker
-        console.log("rust");
+        console.log("rust");        
+        resultImage.src = "rust.png";
+        resultName.innerText = "RUST";
+        resultText.innerText = "asdfasdf";
       } else { //pro user game/social
         console.log("ruby");
+        resultImage.src = "ruby.png";
+        resultName.innerText = "Ruby";
+        resultText.innerText = "asdfasdf";
       }
     } else if (q3Number === 1){ //pro system
       if (q5Number === 0) { //pro system games
         console.log("c#");
+        resultImage.src = "csharp.png";
+        resultName.innerText = "C#";
+        resultText.innerText = "asdfasdf";
       } else { //pro system tinker/social
         console.log("rust");
+        resultImage.src = "rust.png";
+        resultName.innerText = "RUST";
+        resultText.innerText = "asdfasdf";
       }
     } else { //pro game
       console.log("c#");
+      resultImage.src = "csharp.png";
+      resultName.innerText = "C#";
+      resultText.innerText = "asdfasdf";
     }
   }
 }
@@ -157,7 +195,6 @@ function resetForm(){
 
   selectedDiv.className = "hidden";
   initialQuestion.className = "current-active";
-  questionDiv.innerHTML =  initialQuestion.innerHTML;
   nextButton.innerText = "Start";
 }
 
